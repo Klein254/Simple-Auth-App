1a05b94ec52caa4196ebbe7961dc0ffd26511936
Klein254 <rayobiero62@gmail.com>
2023-04-18 12:38:36 +0300

    First Commit

---
+++ app/src/main/java/com/example/authregobiero/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.authregobiero
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var edtFirstName:EditText
+    lateinit var edtSecondName:EditText
+    lateinit var edtEmail:EditText
+    lateinit var edtPassword:EditText
+    lateinit var btnRegister:Button
+    lateinit var db:SQLiteDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtFirstName = findViewById(R.id.edtFirstName)
+        edtSecondName = findViewById(R.id.edtSecondName)
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        db = openOrCreateDatabase("ObieroDB", Context.MODE_PRIVATE, null)
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(FirstName VARCHAR, SecondName VARCHAR, Email VARCHAR, Password VARCHAR)")
+
+        btnRegister.setOnClickListener {
+
+            var FirstName_edt = edtFirstName.text.toString().trim()
+            var SecondName_edt = edtSecondName.text.toString().trim()
+            var Email_edt = edtEmail.text.toString().trim()
+            var Password_edt = edtPassword.text.toString().trim()
+
+
+            if (FirstName_edt.isEmpty() || SecondName_edt.isEmpty() || Email_edt.isEmpty() || Password_edt.isEmpty()){
+                Toast.makeText(this, "Cannot Submit an Empty Field", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                db.execSQL("INSERT INTO users VALUES('\"+FirstName_edt+\"', '\"+SecondName_edt+\"', '\"+Email_edt+\"', '\"+Password_edt+\"')")
+
+                Toast.makeText(this, "User Created Successfully", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+    }
+}
